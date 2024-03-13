940b1df3a9ec218a943c5f06fc8bb81d7ed30699
adhfarg <153331802+adhfarg@users.noreply.github.com>
2024-03-13 14:16:31 -0400

    ”completed”

--- server/seed.py
+++ server/seed.py
@@ -1,33 +1,24 @@
-#!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
-
-from app import app
-from models import db, Message
+from app import app, db, Message
 
 fake = Faker()
 
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
-
 def make_messages():
-
     Message.query.delete()
-    
+
     messages = []
 
     for i in range(20):
         message = Message(
             body=fake.sentence(),
-            username=rc(usernames),
+            username=rc(["Alice", "Bob", "Charlie"]),
         )
         messages.append(message)
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
